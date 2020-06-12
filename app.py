4e1cafe9bad5a37979cdd82b08b3347e33d61af4
Sam-Bonfire <pprathamesh98@gmail.com>
2020-06-13 00:07:47 +0530

    Started to add Login Functionality to the app

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+import config
+
+db = SQLAlchemy()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = config.DB_ENGINE + '://' + config.DB_USERNAME + ':' + config.DB_PASSWORD + \
+        '@' + config.DB_HOST + ':' + config.DB_PORT + '/' + config.DB_NAME
+
+    from routes import app as app_blueprint
+    app.register_blueprint(app_blueprint)
+
+    db.init_app(app)
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    from models import User
+
+    @login_manager.user_loader
+    def load_user(user_login):
+        return User.query.get(user_login)
+
+    return app
+
+
+if __name__ == '__main__':
+    create_app().run()
--- routes.py
+++ routes.py
@@ -1,11 +1,60 @@
-from . import db
-from flask import Blueprint, render_template
+from flask_login import login_user, login_required, current_user
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+
+from models import User
+from app import db
 
 app = Blueprint('app', __name__)
 
 
 @app.route("/")
-@app.route("/index")
 @app.route("/home")
 def index():
     return render_template("base.html", index=True)
+
+
+@app.route("/index")
+@login_required
+def ind():
+    return current_user.user_login
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'GET':
+        return render_template('signup.html')
+    elif request.method == 'POST':
+        user_login = request.form.get('login')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(user_login=user_login).first()
+
+        if user:
+            return redirect(url_for('app.signup'))
+
+        new_user = User(user_login=user_login, password=generate_password_hash(password, method='sha256'))
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('app.login'))
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    if request.method == 'POST':
+        user_login = request.form.get('login')
+        password = request.form.get('password')
+        remember = True if request.form.get('remember') else False
+
+        user = User.query.filter_by(user_login=user_login).first()
+
+        if not user or not check_password_hash(user.password, password):
+            flash('Please check your login details and try again.')
+            return redirect(url_for('app.login'))
+
+        login_user(user, remember=remember)
+        return redirect(url_for('app.index'))

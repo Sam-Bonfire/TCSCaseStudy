3114ddcac850830c6af4ab2dd39e9de86772ad11
Sam-Bonfire <pprathamesh98@gmail.com>
2020-06-12 13:58:25 +0530

    Created the database models

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+import config
+
 app = Flask(__name__)
 
 db = SQLAlchemy()
@@ -11,5 +13,18 @@ def hello_world():
     return 'Hello World!'
 
 
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = config.DB_ENGINE + '://' + config.DB_USERNAME + ':' + config.DB_PASSWORD
+    '@' + config.DB_HOST + ':' + config.DB_PORT + '/'
+    config.DB_NAME
+
+    db.init_app(app)
+    return app
+
+
 if __name__ == '__main__':
-    app.run()
+    create_app().run()
--- models.py
+++ models.py
@@ -4,9 +4,34 @@ from app import db
 
 
 class User(UserMixin, db.Model):
-    __tablename__ = 'customer'
+    __tablename__ = 'userstore'
+    login = db.Column(db.String(35), primary_key=True)
+    password = db.Column(db.String(35))
+
+
+class Customer(db.Model):
     ws_ssn = db.Column(db.Integer, unique=True)
     ws_cust_id = db.Column(db.Integer, primary_key=True)
     ws_name = db.Column(db.String(20))
     ws_adrs = db.Column(db.String(50))
     ws_age = db.Column(db.Integer)
+
+
+class Account(db.Model):
+    act_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    ws_cust_id = db.Column(db.Integer)
+    ws_acct_type = db.Column(db.String(1))
+    ws_acct_balance = db.Column(db.Integer)
+    ws_acct_crdate = db.Column(db.Date)
+    ws_acct_lasttrdate = db.Column(db.Date)
+    ws_acct_duration = db.Column(db.Integer)
+
+
+class Transaction(db.Model):
+    trxn_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    ws_cust_id = db.Column(db.Integer)
+    ws_acct_type = db.Column(db.String(1))
+    ws_amt = db.Column(db.Integer)
+    ws_trxn_date = db.Column(db.Date)
+    ws_src_typ = db.Column(db.String(1))
+    ws_tgt_typ = db.Column(db.String(1))
